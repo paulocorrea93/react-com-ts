2807489b666c30efdf98104b59dcb9dbbeab6a50
Paulo <paulocorreapc93@gmail.com>
2023-08-26 19:21:58 -0400

    variáveis de ts em react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+  //variáveis
+  const name: string = "Paulo";
+  const age: number = 30;
+  const isWorking: boolean = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Typescript com React</h1>
+      <h2>nome: {name}</h2>
+      <p>idade: {age}</p>
+      {isWorking && <p>Está trabalhando</p>}
     </div>
   );
 }
