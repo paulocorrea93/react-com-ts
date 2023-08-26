55e8576297dd80e7f60c383cf61942172fdfc22c
Paulo <paulocorreapc93@gmail.com>
2023-08-26 19:24:35 -0400

    funções ts em react

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,20 @@ function App() {
   const age: number = 30;
   const isWorking: boolean = false;
 
+  //funções
+
+  const userGreeting = (name: string) => {
+    return `olá ${name}`
+  }
+
   return (
     <div className="App">
       <h1>Typescript com React</h1>
       <h2>nome: {name}</h2>
       <p>idade: {age}</p>
       {isWorking && <p>Está trabalhando</p>}
+      <h3>{userGreeting(name)}</h3>
+      {/* <h3>{userGreeting(age)}</h3> NÃO FUNCIONA PORQUE O PARAMETRO ACEITO DEVE SER STRING */}
     </div>
   );
 }
